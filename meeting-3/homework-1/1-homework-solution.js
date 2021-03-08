3a3d67c2dfc0cc0144c8e17f4bd94e89e23253c9
George <george.batselashvili@yahoo.com>
2021-03-09 01:48:25 +0400

    homework-2

--- meeting-3/homework-1/1-homework-solution.js
+++ meeting-3/homework-1/1-homework-solution.js
@@ -1,94 +1,54 @@
 class Vehicle {
-	constructor(make,model){
-  	this.make=make;
-    this.model=model;
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
   }
-
 }
 
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model);
+    this.owners = [];
+    this.year = year;
+  }
 
-class Car extends Vehicle{
-		constructor(year){
-     super();
-     this.owners = [];
-     this.year = year;
-    }
-		
-   
-    getCarInfo() {
-        return `${this.make} ${this.model} released in ${this.year}`
-    };
-    addOwner(owner) {
-        let index = this.owners.indexOf(owner);
-        if (index === -1) {
-            return this.owners.push(owner)
-        } 
-    };
-
-    removeOwner(owner) {
-        let index = this.owners.indexOf(owner);
-        if (index > -1) {
-            return this.owners.splice(index, 1);
-        }
-    };
-
-    getOwnersCount() {
-        return this.owners.length;
-    };
-
-    getOwnerNames () {
-        return this.owners.map(owner => owner.fullName())
-    };
-
-    getFullInfo () {
-        return `${this.make} ${this.model} from ${this.year}. ${this.getOwnersCount()} person owns this car. These are - ${this.getOwnerNames().join( )}`
-
-    }
-
-
+  getCarInfo = () => `${this.make} ${this.model} released in ${this.year}`;
+  addOwner = (owner) => this.owners.indexOf(owner) === -1 ? this.owners.push(owner) : this.owners;
+  removeOwner = (owner) =>(this.owners = this.owners.filter((item) => item !== owner));
+  getOwnersCount = () => this.owners.length;
+  getOwnerNames = () => this.owners.map((owner) => owner.fullName());
+  getFullInfo() {
+    return `${this.make} ${this.model} from ${
+      this.year
+    }. ${this.getOwnersCount()} person owns this car. These are - ${this.getOwnerNames().join()}`;
+  }
 }
 
 class Person {
-		
-    constructor(name, surname, age, gender, cars = []){
-    	 this.cars = cars;
-   	   this.name = name;
-       this.surname = surname;
-       this.age = age;
-       this.gender = gender;
-    }
-		
-    fullName () {
-        return `${this.name} ${this.surname}`;
-    }
-    countCars () {
-        return this.cars.length;
-    }
-    buysCar(car) {
-        this.cars.push(car);
-        let index = car.owners.indexOf(this);
-        if (index === -1) {
-            car.owners.push(this);
-        }
+  constructor(name, surname, age, gender, cars = []) {
+    this.cars = cars;
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.gender = gender;
+  }
 
-    }
-    sellsCar(car) {
-        let carIndex = this.cars.indexOf(car);
-        if (carIndex > -1) {
-            this.cars.splice(carIndex, 1)
-        }
-        let ownerIndex = car.owners.indexOf(this);
-        if (ownerIndex > -1) {
-            car.owners.splice(ownerIndex, 1)
-        }
-    }
-    getAllCarsInfo() {
-        return `${this.name} owns these cars: ${this.cars.map(car=>car.getCarInfo()).join( )}`
-    }
+  fullName = () => `${this.name} ${this.surname}`;
+  countCars = () => this.cars.length;
+  buysCar = (car) => {
+    this.cars.push(car);
+    let index = car.owners.indexOf(this);
+    index === -1 ? car.owners.push(this) : car.owners;
+  };
+  sellsCar = (car) => {
+    this.cars = this.cars.filter((item) => item !== car);
+    car.owners = car.owners.filter((item) => item !== this);
+  };
+  getAllCarsInfo = () =>
+    `${this.name} owns these cars: ${this.cars
+      .map((car) => car.getCarInfo())
+      .join()}`;
 }
-
-
-
 
 let daniel916 = new Person("Daniel", "Barbakadze", 21, "M", []);
 let ilona = new Person("Elon", "Musk", 30, "M");
@@ -100,7 +60,6 @@ daniel916.buysCar(stodevianosto); // adds passed car
 daniel916.sellsCar(duti_picoti); // removes passed car
 ilona.buysCar(stodevianosto); // adds passed car
 ilona.buysCar(duti_picoti); // adds passed car
-
 console.log({
   daniel: {
     fullName: daniel916.fullName(),
